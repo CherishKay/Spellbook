8369fb3e7d0f63059c78cabf08f4eb7f2d885127
CherishKay <122293300+CherishKay@users.noreply.github.com>
2023-06-02 14:01:37 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let disappear= document.querySelector(".disappear");
+let first = document.querySelector(".spell1");
+
+disappear.onclick= function(){
+first.style.display="none";
+}
+
+function myFunction() {
+  document.getElementById("spellBook").reset();
+}
